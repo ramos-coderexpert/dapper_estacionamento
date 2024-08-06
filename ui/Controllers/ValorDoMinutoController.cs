8ad3d098c36f72a8e1cc6f7e967d14a995093292
Lucas Ramos <lucas.ramos.pessoa@hotmail.com>
2024-08-06 19:57:28 -0300

    Criando , Excluindo e Alterando registro com Dapper

--- ui/Controllers/ValorDoMinutoController.cs
+++ ui/Controllers/ValorDoMinutoController.cs
@@ -21,4 +21,48 @@ public class ValorDoMinutoController : Controller
 
         return View(valores);
     }
+
+    [HttpGet("novo")]
+    public IActionResult Novo()
+    {
+        return View();
+    }
+
+    [HttpPost("criar")]
+    public IActionResult Criar([FromForm] ValorDoMinuto valorDoMinuto)
+    {
+        string sql = "INSERT INTO valores (Minutos, Valor) VALUES (@Minutos, @Valor)";
+        _connection.Execute(sql, new { Minutos = valorDoMinuto.Minutos, Valor = valorDoMinuto.Valor });
+
+        return Redirect("/valores");
+    }
+
+    [HttpGet("{id}/editar")]
+    public IActionResult Editar([FromRoute] int id)
+    {
+        string sql = "SELECT * FROM valores WHERE Id = @Id";
+        var valor = _connection.Query<ValorDoMinuto>(sql, new { Id = id }).FirstOrDefault();
+
+        return View(valor);
+    }
+
+    [HttpPost("{id}/alterar")]
+    public IActionResult Alterar([FromRoute] int id, [FromForm] ValorDoMinuto valorDoMinuto)
+    {
+        valorDoMinuto.Id = id;
+
+        string sql = "UPDATE valores SET Minutos = @Minutos, Valor = @Valor WHERE Id = @Id";
+        _connection.Execute(sql, valorDoMinuto);
+
+        return Redirect("/valores");
+    }
+
+    [HttpPost("{id}/excluir")]
+    public IActionResult Excluir([FromRoute] int id)
+    {
+        string sql = "DELETE FROM valores WHERE Id = @Id";
+        _connection.Execute(sql, new { Id = id });
+
+        return Redirect("/valores");
+    }
 }
--- ui/Models/ValorDoMinuto.cs
+++ ui/Models/ValorDoMinuto.cs
@@ -1,5 +1,8 @@
-﻿namespace Dapper_estacionamento.Models
+﻿using System.Diagnostics;
+
+namespace Dapper_estacionamento.Models
 {
+    [DebuggerDisplay("Valor {Id}: {Minutos} valem R${Valor}")]
     public class ValorDoMinuto
     {
         public int Id { get; set; }
