8ad9ca336e185dd9a90ad04e23b0a1b0290820bf
Lucas Ramos <lucas.ramos.pessoa@hotmail.com>
2024-08-24 22:05:25 -0300

    Executando Procedures e Views

--- ui/Controllers/ValorDoMinutoController.cs
+++ ui/Controllers/ValorDoMinutoController.cs
@@ -19,6 +19,34 @@ public class ValorDoMinutoController : Controller
     {
         var valores = _connection.Query<ValorDoMinuto>("SELECT * FROM valores");
 
+        // Executando Procedure, forma simples
+        //var parameters = new DynamicParameters();
+        //parameters.Add("@idCliente", 1);
+        //var tickets = _connection.Query<TicketComCliente>("ObterTicketsPorCliente", parameters, commandType: CommandType.StoredProcedure);
+
+
+        // Executando Procedure, forma completa
+        //var parameters = new DynamicParameters();
+        //parameters.Add("@idCliente", 1);
+        //var tickets = _connection.Query<Ticket, string, TicketComCliente>("ObterTicketsPorCliente", (ticket, nomeCliente) =>
+        //{
+        //    return new TicketComCliente { Ticket = ticket, NomeCliente = nomeCliente };
+        //},
+        //parameters, splitOn: "NomeCliente", commandType: CommandType.StoredProcedure);
+
+
+        // Executando View, forma simples
+        //var sqlView = "SELECT * FROM VistaTicketComClientes";
+        //var ticketsComClientes = _connection.Query<TicketComCliente>(sqlView);
+
+
+        // Executando View, forma completa
+        //var ticketsComClientes = _connection.Query<Ticket, string, TicketComCliente>("SELECT * FROM VistaTicketComClientes",
+        //(ticket, nomeCliente) =>
+        //{
+        //    return new TicketComCliente { Ticket = ticket, NomeCliente = nomeCliente };
+        //}, splitOn: "NomeCliente");
+
         return View(valores);
     }
 
---
+++ ui/Models/TicketComCliente.cs
@@ -0,0 +1,8 @@
+﻿namespace Dapper_estacionamento.Models
+{
+    public class TicketComCliente
+    {
+        public Ticket Ticket { get; set; }
+        public string NomeCliente { get; set; }
+    }
+}
